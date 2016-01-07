2fca4851fb5f906d77bb27afec0008d3e808f6e9
beginor <beginor@qq.com>
2016-01-07 15:12:15 +0800

    gulp task.

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,43 @@
 var gulp = require('gulp');
-gulp.task('default', function () {
-    //gulp.src("bower_components/bootstrap/dist/**/*").pipe().dest("www/libs/bootstrap");
+var copy = require('gulp-copy');
+
+gulp.task('angular2', function () {
+    gulp.src([
+        'node_modules/angular2/bundles/angular2-polyfills.js',
+        'node_modules/angular2/bundles/angular2-polyfills.min.js',
+        'node_modules/angular2/bundles/angular2.js',
+        'node_modules/angular2/bundles/angular2.min.js',
+        'node_modules/angular2/bundles/router.js',
+        'node_modules/angular2/bundles/router.min.js',
+        'node_modules/angular2/bundles/http.js',
+        'node_modules/angular2/bundles/http.min.js'
+    ]).pipe(gulp.dest('www/libs/angular2'));
 });
+
+gulp.task('bootstrap', function () {
+    gulp.src('bower_components/bootstrap/dist/**/*.*')
+        .pipe(gulp.dest('www/libs/bootstrap'));
+});
+
+gulp.task('jquery', function () {
+    gulp.src('bower_components/jquery/dist/**/*.*')
+        .pipe(gulp.dest('www/libs/jquery'));
+});
+
+gulp.task('rxjs', function () {
+    gulp.src([
+        'node_modules/rxjs/bundles/Rx.js',
+        'node_modules/rxjs/bundles/Rx.min.js',
+        'node_modules/rxjs/bundles/Rx.min.js.map'
+    ]).pipe(gulp.dest('www/libs/rxjs'));
+});
+
+gulp.task('systemjs', function () {
+    gulp.src([
+        'node_modules/systemjs/dist/system.src.js',
+        'node_modules/systemjs/dist/system.js',
+        'node_modules/systemjs/dist/system.js.map'
+    ]).pipe(gulp.dest('www/libs/systemjs'));
+});
+
+gulp.task('default', ['angular2', 'bootstrap', 'jquery', 'rxjs', 'systemjs']);
